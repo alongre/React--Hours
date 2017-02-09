8c0ba6c3495c3a3653ec2404759dc9c082aa5556
Alon Green <alongre@gmail.com>
2017-02-09 11:43:32 +0200

    added styles:

--- src/app/components/hours/hours.js
+++ src/app/components/hours/hours.js
@@ -1,16 +1,21 @@
 import h from 'react-hyperscript';
 import { Form, FormGroup, Label, FormControl, ControlLabel } from 'react-bootstrap';
+import './hours.scss';
+
 
 
 export const Hours = () => {
   return  (
-    h(FormGroup, [
-      h(ControlLabel, {bsStyle: 'primary'}, 'Start'),
-      h(Label,' '),
+    h(Form,{ inline: true}, [
+    h(FormGroup,{ bsSize:'sm'}, [
+      h('h3', [
+      h(Label, {bsStyle: 'primary'}, 'Start:'),
+      h(Label, { className: 'empty_label'},' '),
       h(FormControl, { type: 'time', defaultValue: '08:00'}),
-      h(Label,' '),
-      h(Label,'End:'),
-      h(Label,' '),
+      h(Label, { className: 'empty_label'},' '),
+      h(Label, {bsStyle: 'primary'}, 'End:'),
       h(FormControl, { type: 'time', defaultValue: '17:00'}),
+      ]),
+  ])
   ]));
 };
