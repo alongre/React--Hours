5f09e4ed751c2fed7e4cf639d0f3a7ede2176194
ZINY Gil <gil.ziny@intergraph.com>
2017-02-09 08:58:14 +0200

    add style

--- src/app/components/hours/hours.js
+++ src/app/components/hours/hours.js
@@ -5,12 +5,12 @@ import { Form, FormGroup, Label, FormControl, ControlLabel } from 'react-bootstr
 export const Hours = () => {
   return  (
     h(FormGroup, [
-      h(ControlLabel, {'bsStyle': 'primary'}, ['Start']),
+      h(ControlLabel, {bsStyle: 'primary'}, 'Start'),
       h(Label,' '),
       h(FormControl, { type: 'time', defaultValue: '08:00'}),
       h(Label,' '),
       h(Label,'End:'),
       h(Label,' '),
-      h(FormControl, { type: 'time', defaultValue: '17:00'})
+      h(FormControl, { type: 'time', defaultValue: '17:00'}),
   ]));
 };
