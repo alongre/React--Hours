249433d1a3a84fefeae211d9c3a0af62fc69e3a7
Alon Green <alongre@gmail.com>
2017-02-13 15:30:39 +0200

    Hours accordion

--- src/app/components/hours/hours.js
+++ src/app/components/hours/hours.js
@@ -1,21 +1,43 @@
 import h from 'react-hyperscript';
-import { Form, FormGroup, Label, FormControl, ControlLabel } from 'react-bootstrap';
+import { PropTypes } from 'react';
+import { Form, FormGroup, Label, FormControl, Accordion, Panel, Col } from 'react-bootstrap';
 import './hours.scss';
 
-
-
-export const Hours = () => {
-  return  (
-    h(Form,{ inline: true}, [
-    h(FormGroup,{ bsSize:'sm'}, [
-      h('h3', [
-      h(Label, {bsStyle: 'primary'}, 'Start:'),
-      h(Label, { className: 'empty_label'},' '),
-      h(FormControl, { type: 'time', defaultValue: '08:00'}),
-      h(Label, { className: 'empty_label'},' '),
-      h(Label, {bsStyle: 'primary'}, 'End:'),
-      h(FormControl, { type: 'time', defaultValue: '17:00'}),
+export const Hours = ({ Shift }) => {
+  return (
+    h(Accordion, [
+      h(Panel, { bsStyle: 'success', header: '13.2.2017: 9:05 hours', eventKey: '1' }, [
+        h('.time', [Shift.map(shift =>
+        h(Form, { inline: true, key: shift.id }, [
+          h(Col, { sm: 1 }, [
+            h(FormGroup, { controlId: '1' }, [
+              h(Col, { sm: 12 }, [
+                h(Label, { bsStyle: 'primary', className: 'labels' }, 'Start:'),
+              ]),
+              h(Col, { sm: 12 }, [
+                h(FormControl, { type: 'time', defaultValue: shift.Start, bsSize: 'md' }),
+              ]),
+            ]),
+          ]),
+          h(Col, { sm: 0 }, [
+            h(FormGroup, { controlId: '2' }, [
+              h(Col, { sm: 12 }, [
+                h(Label, { bsStyle: 'primary', className: 'labels' }, 'End:'),
+              ]),
+              h(Col, { sm: 12 }, [
+                h(FormControl, { type: 'time', defaultValue: shift.End, bsSize: 'md' }),
+              ]),
+            ]),
+          ]),
+        ])
+      )]),
       ]),
-  ])
-  ]));
+    ]));
+};
+
+Hours.propTypes = {
+  Shift: PropTypes.arrayOf(PropTypes.shape({
+    Start: PropTypes.string.isRequired,
+    End: PropTypes.string.isRequired,
+  }).isRequired),
 };
